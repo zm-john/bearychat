342636e21d1bca18183f827b2b9a666de0dcca5d
John <quhang@weipei.cc>
2018-02-24 09:50:23 +0800

    markdown field fail

--- src/Message.php
+++ src/Message.php
@@ -29,7 +29,7 @@ class Message
 
     public function markdown(bool $need = true)
     {
-        $this->message['notification'] = $need;
+        $this->message['markdown'] = $need;
         return $this;
     }
 
